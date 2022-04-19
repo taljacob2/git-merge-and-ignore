648bc20dbc5e97d3d30ce9e14464d2ab09829ef5
taljacob2 <taljacob2@gmail.com>
2022-04-19 09:29:06 +0300

    Sketch Of `pre-merge-merge-and-ignore.sh` Works
    Signed-off-by: taljacob2 <taljacob2@gmail.com>

--- pre-merge-merge-and-ignore.sh
+++ pre-merge-merge-and-ignore.sh
@@ -1,11 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
-readfile(){
-    lineList=()
+: '
+Description:
+Reads a file line-by-line to and returns it as an array of strings.
+
+Parameters:
+$1: FILE_NAME_TO_READ: Path of the file to read.
+
+Example Of Use:
+```
+file=$(readFile FILE_NAME_TO_READ)
+for line in $file; do
+    echo $line
+done
+```
+'
+readFile(){
+    local lineList=()
     while IFS= read -r line || [[ -n "$line" ]]; do
         lineList+=("$line")
     done < "$1"
     echo "${lineList[@]}"
 }
 
-echo ($(readfile .gitmergeandignore))
+file=$(readFile .gitmergeandignore)
+for e in $file; do
+    echo $e
+done
