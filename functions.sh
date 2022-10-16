ea88cefa52991ac2a7443569b4d8c491319bed21
Tal Jacob <taljacob2@gmail.com>
2022-10-16 17:25:57 +0300

    Support Ignoring For Conflicted Files
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- functions.sh
+++ functions.sh
@@ -23,3 +23,28 @@ readFile(){
     done < "$1"
     echo "${lineList[@]}"
 }
+
+: '
+Description:
+Resolves a conflicted file with a certain strategy: ours|theirs|union
+
+Parameters:
+$1: STRATEGY: Pick a strategy between `ours|theirs|union` to resolve the file
+              with.
+$2: FILE_PATH: The path to the conflicted file to resolve.
+'
+resolveConflictedFile() {
+  STRATEGY="$1"
+  FILE_PATH="$2"
+
+  git show :1:"$FILE_PATH" > ./tmp.base
+  git show :2:"$FILE_PATH" > ./tmp.ours
+  git show :3:"$FILE_PATH" > ./tmp.theirs
+
+  git merge-file "$STRATEGY" -p ./tmp.ours ./tmp.base ./tmp.theirs > "$FILE_PATH"
+  git add "$FILE_PATH"
+
+  rm ./tmp.base
+  rm ./tmp.ours
+  rm ./tmp.theirs
+}
--- merge-and-ignore.sh
+++ merge-and-ignore.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 source ./git-merge-and-ignore/functions.sh
 
@@ -20,7 +20,7 @@ NUMBER_OF_ARGS=${#ARGS[@]}
 MINIMUM_NUMBER_OF_ARGS=1
 if [ $NUMBER_OF_ARGS -lt $MINIMUM_NUMBER_OF_ARGS ]; then
     echo "Wrong number of arguments. Should be at least $MINIMUM_NUMBER_OF_ARGS, but was $NUMBER_OF_ARGS.";
-    exit 1;    
+    exit 1;
 fi
 
 # Default state is to enable read of wildcards from `MERGE_AND_IGNORE_FILE_NAME_TO_READ`.
@@ -45,7 +45,7 @@ do
       -*)
           echo "Error: Unknown option: $1" >&2
           # Or call function `display_help`
-          exit 1 
+          exit 1
           ;;
       *)  # No more options
           break
@@ -79,21 +79,37 @@ echo $LOG_HALF_BOUNDARY_SHORT START MERGE-AND-IGNORE $LOG_HALF_BOUNDARY
 HAS_MERGE_STARTED_MESSAGE=$(git merge ${ARGS[0]} --no-ff --no-commit)
 HAS_MERGE_STARTED_EXIT_CODE=$?
 
+# Get all conflicted files as a long string.
+conflictedFilesAsString=`git diff --name-only --diff-filter=U`
+
+# Convert `conflictedFilesAsString` to a list of strings.
+mapfile -t conflictedFiles <<< "$conflictedFilesAsString"
+
 for wildcardToIgnore in $ALL_WILDCARDS_TO_IGNORE; do
+
+    # If `$conflictedFiles[@]` contains `$wildcardToIgnore`:
+    if printf '%s\0' "${conflictedFiles[@]}" | grep -Fxqz -- "$wildcardToIgnore" ; then
+        isWildcardToIgnoreContainedWithinConflictedFiles=true
+
+        # Choose arbitrarily to take a version of the file (ours/theirs/union).
+        resolveConflictedFile --union "$wildcardToIgnore" > /dev/null 2>&1
+    else
+        isWildcardToIgnoreContainedWithinConflictedFiles=false
+    fi
+
     echo Ignoring \"$wildcardToIgnore\"  # Output logs.
 
     git reset HEAD $wildcardToIgnore > /dev/null 2>&1   # Unstaging the current wildcard.
     git checkout -- $wildcardToIgnore > /dev/null 2>&1  # Reverting working-directory of the current wildcard.
+
+    # In case `$wildcardToIgnore` was conflicted, clean its `.orig` file.
+    [ "$isWildcardToIgnoreContainedWithinConflictedFiles" == true ] && git clean -f "$wildcardToIgnore.orig" > /dev/null 2>&1
 done
 
 echo $LOG_HALF_BOUNDARY_SHORT FINISH MERGE-AND-IGNORE $LOG_HALF_BOUNDARY_SHORT
 
-if [[ $HAS_MERGE_STARTED_EXIT_CODE == 0 ]]; then
-    # DEVELOPER NOTE: Choose one of the following options:
-    GIT_EDITOR=true git merge --continue  # DEVELOPER NOTE: Enable this line to enable `--no-edit`.
-    # git merge --continue  # DEVELOPER NOTE: Enable this line to disable `--no-edit`.
-else
-    echo $HAS_MERGE_STARTED_MESSAGE
-fi
+# DEVELOPER NOTE: Choose one of the following options:
+GIT_EDITOR=true git merge --continue  # DEVELOPER NOTE: Enable this line to enable `--no-edit`.
+# git merge --continue  # DEVELOPER NOTE: Enable this line to disable `--no-edit`.
 
 exit
